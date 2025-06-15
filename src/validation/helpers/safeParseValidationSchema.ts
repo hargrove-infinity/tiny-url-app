e26dca0c62b0a763b9ff449171b704198c055886
hargrove <hargrove.infinity@gmail.com>
2025-06-15 18:57:51 +0300

    reorganized validation folder

--- src/validation/helpers/safeParseValidationSchema.ts
+++ src/validation/helpers/safeParseValidationSchema.ts
@@ -1,14 +1,10 @@
 import { ZodType } from "zod";
-
-interface IArgs<T extends ZodType<any, any, any>> {
-  schema: T;
-  data: unknown;
-}
+import { ISafeParseValidationSchemaArgs } from "./types";
 
 export function safeParseValidationSchema<T extends ZodType<any, any, any>>({
   schema,
   data,
-}: IArgs<T>): ReturnType<T["parse"]> | null {
+}: ISafeParseValidationSchemaArgs<T>): ReturnType<T["parse"]> | null {
   const result = schema.safeParse(data);
   return !result.success ? null : result.data;
 }
