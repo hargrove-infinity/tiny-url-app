3f03f69bb9e81bb0d3db620b60c8689b2de43f06
hargrove <hargrove.infinity@gmail.com>
2025-07-05 17:15:02 +0300

    added calculateDelay helper function

--- src/util/retry/types.ts
+++ src/util/retry/types.ts
@@ -8,3 +8,16 @@ export type Action<R, E> = () => Promise<[R, undefined] | [undefined, E]>;
 export type RetryReturnType<R, E> = () => Promise<
   [R, undefined] | [undefined, E]
 >;
+
+export enum BackoffStrategy {
+  FIXED = "FIXED",
+  EXPONENTIAL = "EXPONENTIAL",
+  LINEAR = "LINEAR",
+}
+
+export interface ICalculateDelayArgs {
+  attempt: number;
+  baseDelay: number;
+  maxDelay: number;
+  backoffStrategy: BackoffStrategy;
+}
